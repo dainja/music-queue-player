278c8d87249c3bdadfb81ecd6845692accf15e44
danielnajafi <daniel.najafizadeh@gmail.com>
2021-02-16 13:33:05 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+import { SearchInputComponent } from './search/components/search-input/search-input.component';
+import { SearchListComponent } from './search/components/search-list/search-list.component';
+import { SearchContainerComponent } from './search/container/search-container/search-container.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchInputComponent,
+    SearchListComponent,
+    SearchContainerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MaterialModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/search/components/search-input/search-input.component.ts
@@ -0,0 +1,35 @@
+import { AfterViewInit, Component, ElementRef, OnInit, Output, ViewChild, EventEmitter } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, map, pluck } from 'rxjs/operators';
+
+
+
+@Component({
+  selector: 'app-search-input',
+  templateUrl: './search-input.component.html',
+  styleUrls: [ './search-input.component.css' ]
+})
+export class SearchInputComponent implements AfterViewInit {
+
+  @ViewChild('input') inputElement: ElementRef;
+  @Output() search: EventEmitter<string> = new EventEmitter<string>();
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    fromEvent(this.inputElement.nativeElement, 'keyup')
+      .pipe(
+        debounceTime(500),
+        pluck('target', 'value'),
+        distinctUntilChanged(),
+        filter((value: string) => value.length > 3),
+        map((value) => value)
+      )
+      .subscribe(value => {
+        this.search.emit(value)
+      })
+  }
+
+}

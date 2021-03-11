ae25df6abb1f5a1d4d41311540be571fd7d8cc07
danielnajafi <daniel.najafizadeh@gmail.com>
2021-03-11 14:14:41 +0100

    changed firebase hosting

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyD2DHLrzdYGlp9fItkL1yjkClxhTZXhKow",
-    authDomain: "party-queue-angular.firebaseapp.com",
-    projectId: "party-queue-angular",
-    storageBucket: "party-queue-angular.appspot.com",
-    messagingSenderId: "362176426279",
-    appId: "1:362176426279:web:5a76b6835cfceb40f17902",
-    measurementId: "G-PMQGJW41Z7"
+    apiKey: "AIzaSyA1s02_J7Rq1yqO4wqI6RBnRDTNh2DgQyA",
+    authDomain: "lgnd-music-player.firebaseapp.com",
+    projectId: "lgnd-music-player",
+    storageBucket: "lgnd-music-player.appspot.com",
+    messagingSenderId: "538058197517",
+    appId: "1:538058197517:web:37541dd95cc1ed2d986344",
+    measurementId: "G-898LY09VBR"
   }
 };
 
